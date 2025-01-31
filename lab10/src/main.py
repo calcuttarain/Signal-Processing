6e246e8b1a87ad3eff2b0e95c6f2c14b8dd4a4bf
Tudor Pistol <116034514+calcuttarain@users.noreply.github.com>
2025-01-31 23:48:58 +0200

    final project

--- lab10/src/main.py
+++ lab10/src/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from statsmodels.tsa.ar_model import AutoReg
 
 def save_plot(time, signal, title, filename):
     plt.figure(figsize=(16, 9))
@@ -11,6 +12,7 @@ def save_plot(time, signal, title, filename):
     plt.show()
     plt.clf()
 
+# 1
 polinom = {'a': 3, 'b': 5, 'c': 4}
 freq = [12, 18]
 time = np.linspace(0, 1, 1000)
@@ -21,4 +23,18 @@ white_noise = np.random.normal(0, 0.3, 1000)
 
 signal = trend + seasonal + white_noise
 
+# 2
+p = 20  
+model = AutoReg(signal, lags=p)
+model_fitted = model.fit()
 
+reconstructed_signal = model_fitted.fittedvalues
+
+plt.figure(figsize=(16, 9))
+plt.plot(time, signal, label="Original Signal")
+plt.plot(time[p:], reconstructed_signal, label="Reconstructed Signal", linestyle='--')
+plt.title("Original vs Reconstructed Signal using AR model")
+plt.legend()
+plt.grid()
+plt.tight_layout()
+plt.show()
